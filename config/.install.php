ba1f565aefc31f66c1dd40d65bd81f4c18cb4dc0
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-12 15:35:55 +0300

    Файл конфигурации установки расширения

---
+++ config/.install.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения RosGear.
+ * 
+ * Файл конфигурации установки расширения.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'id'          => 'rg.be.references.desk',
+    'moduleId'    => 'rg.be.references',
+    'name'        => 'References Panel',
+    'description' => 'Panel for displaying references elements',
+    'namespace'   => 'Rg\Backend\References\Desk',
+    'path'        => '/rg/rg.be.references.desk',
+    'route'       => 'desk',
+    'locales'     => ['ru_RU', 'en_GB'],
+    'permissions' => ['any', 'view', 'read', 'info'],
+    'events'      => [],
+    'required'    => [
+        ['php', 'version' => '8.2'],
+        ['app', 'code' => 'RG Workspace'],
+        ['app', 'code' => 'RG CMS'],
+        ['app', 'code' => 'RG CRM'],
+        ['module', 'id' => 'rg.be.references']
+    ]
+];
