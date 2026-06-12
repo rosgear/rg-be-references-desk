de0dcedef7fcf2827a4fd77c469cbf04cfa7bcfc
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-12 15:34:40 +0300

    Файл локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения RosGear.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    '{name}'        => 'References Panel',
+    '{description}' => 'Panel for displaying references elements',
+    '{permissions}' => [
+        'any'  => ['Full access', 'View and read panel items'],
+        'view' => ['View', 'Panel view'],
+        'read' => ['Reading', 'Reading panel items']
+    ]
+];
